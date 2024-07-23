a8a77c0f05c4635153c81d1b47a3e1a16d6de845
Song-Gi-Sung <wlswlek123@naver.com>
2024-07-23 12:50:20 +0900

    first commit

---
+++ src/test/java/sneakers/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package sneakers.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
