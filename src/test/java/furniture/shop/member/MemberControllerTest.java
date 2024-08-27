db1fcc14986ca22e0257079997a3f92c9eb7fb89
Song-Gi-Sung <wlswlek123@naver.com>
2024-08-27 17:24:00 +0900

    edit ignore

--- src/test/java/furniture/shop/member/MemberControllerTest.java
+++ src/test/java/furniture/shop/member/MemberControllerTest.java
@@ -4,25 +4,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import furniture.shop.configure.exception.CustomException;
 import furniture.shop.configure.exception.CustomExceptionCode;
 import furniture.shop.member.constant.MemberGender;
+import furniture.shop.member.dto.MemberInfoDto;
 import furniture.shop.member.dto.MemberJoinDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ActiveProfiles("test")
-@WebMvcTest(controllers = MemberController.class)
+@WebMvcTest(controllers = MemberController.class,
+    excludeAutoConfiguration = SecurityAutoConfiguration.class,
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {OncePerRequestFilter.class}))
 class MemberControllerTest {
 
     @Autowired
@@ -38,7 +45,7 @@ class MemberControllerTest {
 
         given(memberService.joinMember(any())).willReturn(1L);
 
-        mockMvc.perform(post("/member/join")
+        mockMvc.perform(post("/join")
                 .content(new ObjectMapper().writeValueAsString(memberJoinDto))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
@@ -54,7 +61,7 @@ class MemberControllerTest {
 
         given(memberService.joinMember(any())).willThrow(new CustomException(CustomExceptionCode.JOIN_DUPLICATE_EXCEPTION));
 
-        mockMvc.perform(post("/member/join")
+        mockMvc.perform(post("/join")
                 .content(new ObjectMapper().writeValueAsString(memberJoinDto))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
@@ -68,12 +75,59 @@ class MemberControllerTest {
         MemberJoinDto memberJoinDto = getMemberJoinDto();
         memberJoinDto.setEmail("test");
 
-        mockMvc.perform(post("/member/join")
+        mockMvc.perform(post("/join")
                         .content(new ObjectMapper().writeValueAsString(memberJoinDto))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is4xxClientError())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원정보 조회 테스트")
+    void 회원정보_조회_테스트() throws Exception {
+        MemberInfoDto memberInfoDto = getMemberInfoDto();
+
+        given(memberService.getMemberInfo()).willReturn(memberInfoDto);
+
+        mockMvc.perform(get("/member"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원정보 조회 실패 테스트")
+    void 회원정보_조회_실패_테스트() throws Exception {
+        given(memberService.getMemberInfo()).willThrow(new CustomException(CustomExceptionCode.NOT_VALID_ERROR));
+
+        mockMvc.perform(get("/member"))
+                .andExpect(status().is4xxClientError())
+                .andDo(print());
+    }
+
+
+    @Test
+    @DisplayName("회원정보 수정 테스트")
+    void 회원정보_수정_테스트() throws Exception {
+        given(memberService.updateMember(any())).willReturn(getMemberInfoDto());
+
+        mockMvc.perform(patch("/member"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    private static MemberInfoDto getMemberInfoDto() {
+        MemberInfoDto memberInfoDto = new MemberInfoDto();
+        memberInfoDto.setUsername("테스터");
+        memberInfoDto.setPhone("01012345678");
+        memberInfoDto.setEmail("test@test.com");
+        memberInfoDto.setMileage(1092);
+        memberInfoDto.setMemberGender(MemberGender.FEMALE);
+        memberInfoDto.setZipCode("12345");
+        memberInfoDto.setCity("서울시 은평구");
+        memberInfoDto.setStreet("은평대로 123-34");
+
+        return memberInfoDto;
     }
 
     private static MemberJoinDto getMemberJoinDto() {
